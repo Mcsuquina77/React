3bafcb38a7e0a1ea8f2974fe1b9aed12c63d0fe3
Mcsuquina77 <189384118+Mcsuquina77@users.noreply.github.com>
2026-04-14 05:27:21 +0100

    ate a ultima aula

--- classroom/src/components/button/index.tsx
+++ classroom/src/components/button/index.tsx
@@ -1,10 +1,14 @@
 import styles from "./styles.module.css"
+
 type Props = React.ComponentProps<"button"> & {
     name: string
 }
 
 export function Button({ name, ...rest}: Props){
-    return <button className={styles.container} {...rest}>
+   
+
+
+   return <button className={styles.container} {...rest}>
         <span>{name}</span>
         </button>
 }
