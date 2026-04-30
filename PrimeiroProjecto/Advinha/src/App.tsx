860fdc5fc2436c1a8368a49068ae43e017c9eed8
Mcsuquina77 <189384118+Mcsuquina77@users.noreply.github.com>
2026-04-30 09:52:18 +0100

    Added root element with font-size property and box-sizing property to all elements

---
+++ PrimeiroProjecto/Advinha/src/App.tsx
@@ -0,0 +1,20 @@
+import styles from "./app.module.css"
+
+import { Header } from "./components/Header"
+
+export default function App () 
+{
+  function handleRestartGame () {
+    alert("reiniciar o jogo!")
+  }
+
+
+
+  return (
+  <div  className={styles.container}>
+     <main>
+      <Header current={5} max={10} onRestart={handleRestartGame} />
+    </main>
+  </div>
+  )
+}
